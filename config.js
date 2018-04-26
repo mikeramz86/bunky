3c0b2ac9bd3c499d1b9e81f30bbe3f19a8f2af3b
Mike <mikeramzdesign@gmail.com>
2018-04-26 15:47:05 -0700

    config js: updated

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/waxstack';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/bunky_db';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
 exports.PORT = process.env.PORT || 8080

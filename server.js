a0b3ffe99c6cb6b34a7b99569fc1e1908d0e9df3
Mike <mikeramzdesign@gmail.com>
2018-04-26 11:30:58 -0700

    added loggedinjs updated server and userjs

--- server.js
+++ server.js
@@ -1,15 +1,79 @@
 'use strict';
-
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+var path = require('path');
+var logger = require('morgan');
+var users = require('./routes/users');
+var logged_in = require('./routes/logged_in');
+var regaliaTotal = require('./routes/regalia');
+
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
-app.use(express.static('public'));
+app.use(logger('dev'));
 
-if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function () {
-    console.info(`App listening on ${this.address().port}`);
+app.use(express.static(path.join(__dirname, 'public')));
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/auth/', authRouter);
+app.use('/users', users);
+app.use('/logged_in', logged_in);
+
+//main place to find profiles
+app.use('/bunky', bunkyTotal);
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+let server;
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
   });
 }
-
-module.exports = app;
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+module.exports = { app, runServer, closeServer };
---
+++ user.js
@@ -0,0 +1,143 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const passport = require('passport');
+const { User } = require('../models/users');
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+router.get('/', jwtAuth,(req, res) => {
+  try {
+    User.find({}).then(allusers => {
+      res.json({allusers})
+    })
+  } catch(err) {
+    res.json({err})
+  }
+});
+router.get('/for_tests',(req, res) => {
+  try {
+       User.find({}).then(allusers => {
+           res.json({allusers})
+       })
+  } catch(err) {
+      res.json({err})
+  }
+});
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = [ 'password', 'FirstName', 'LastName', 'EmailAddress' ];
+  const missingField = requiredFields.find(field => !(field in req.body));
+  if (missingField) {
+    console.log('missing entity field')
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+//check to see if datatypes are correct
+  const stringFields = [ 'password', 'FirstName', 'LastName', 'EmailAddress'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+  if (nonStringField) {
+    console.log('missing nonstring entity field')
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+  const explicityTrimmedFields = ['password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    password: {
+      min: 6,
+      max: 20
+    }
+  };
+  //making sure psword is correct length
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let {password, FirstName = '', LastName = '', EmailAddress , RentPayment} = req.body;
+  // Username and password come in pre-trimmed, otherwise we throw an error
+  // before this
+  FirstName = FirstName.trim();
+  LastName = LastName.trim();
+  EmailAddress = EmailAddress.trim();
+  password = password.trim();
+
+  return User.find({EmailAddress})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Email Address already taken',
+          location: 'EmailAddress'
+        });
+      }
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        EmailAddress,
+        password: hash,
+        FirstName,
+        LastName,
+        RentPayment
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      console.log('here2', err)
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
+});
+module.exports = router;
